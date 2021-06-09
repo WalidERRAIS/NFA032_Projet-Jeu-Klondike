40bc78587670529624131647bde0e21653b9d28b
VatosLocoos <85027526+VatosLocoos@users.noreply.github.com>
2021-06-09 16:40:39 +0200

    Update PaquetColonne.java

--- src/tp_cartes/PaquetColonne.java
+++ src/tp_cartes/PaquetColonne.java
@@ -1,15 +1,14 @@
 package tp_cartes;
 
-import gestionError.DeplacementImpossibleException;
 
 public class PaquetColonne extends Paquet {
 	//constructeur avec n cartes du paquet distrib
-	public PaquetColonne(PaquetDistributeur p, int n) throws DeplacementImpossibleException {
+	public PaquetColonne(PaquetDistributeur p, int n) {
 		//2 objets/colonne => 1 tas cachees herité et 1 tas visible herité
 		super(p, n); 
 	}
 	//ajoute une carte au sommet du paquet si couleur alterne et valeur precedente
-	public void ajoutCarte(Carte c) throws DeplacementImpossibleException {
+	public void ajoutCarte(Carte c) {
 		//si paquet vide j'ajoute carte que si Roi
 		if (this.isEmpty() && c.getValeur().equals(Valeur.roi))
 			super.ajoutCarte(c);
@@ -18,14 +17,14 @@ public class PaquetColonne extends Paquet {
 		else if (!this.isEmpty() && this.getTop().precedeDiffTeinte(c))
 			super.ajoutCarte(c);
 		else 
-			throw new DeplacementImpossibleException();	
+			throw new IllegalArgumentException("Erreur -> Deplacement Impossible!");	
 	}
 	//ajoute une carte de paquet cache à paquet visible
-	public void ajoutCarteVisible(Carte c) throws DeplacementImpossibleException {
+	public void ajoutCarteVisible(Carte c) {
 		//si paquet visible vide j'ajoute carte de paquet cache
 		if (this.isEmpty())
 			super.ajoutCarte(c);
 		else 
-			throw new DeplacementImpossibleException();
+			throw new IllegalArgumentException("Erreur -> Deplacement Impossible!");
 	}
 }
