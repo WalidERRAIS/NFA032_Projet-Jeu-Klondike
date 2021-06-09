8224235369add828b87c9d13508ab57419d7d467
VatosLocoos <85027526+VatosLocoos@users.noreply.github.com>
2021-06-09 16:41:23 +0200

    Update Pioche.java

--- src/tp_cartes/Pioche.java
+++ src/tp_cartes/Pioche.java
@@ -1,12 +1,11 @@
 package tp_cartes;
 
-import gestionError.*;
-import interfaceGraphique.InterfaceKlondike;
+import interfaceGraphique.*;
 
 public class Pioche extends Paquet {
 	private Paquet defausse; //paquet visible
 	//constructeur
-	public Pioche(PaquetDistributeur p, InterfaceKlondike itp) throws DeplacementImpossibleException {
+	public Pioche(PaquetDistributeur p, InterfaceKlondike itp) {
 		//initialise la partie a piocher avec toutes les cartes restantes du distrib
 		//et creer un paquet vide pour la defausse
 		super(p, p.getNombreCartePaquet());
@@ -20,35 +19,26 @@ public class Pioche extends Paquet {
 	public boolean DefausseEmpty() {
 		return defausse.isEmpty();
 	}
-	//si pioche contient carte deplace carte du haut de la pioche � la defausse
-	public void deplaceCartePiocheDefausse(Pioche p, InterfaceKlondike itp) throws PiocheVideException, DeplacementImpossibleException {
-		if (!p.isEmpty()) {
-			p.getTop().rendVisible();
-			defausse.ajoutCarte(p.removeTop());
-			itp.ajouterUneCarte(defausse.getTop(), 1);
-			if(p.isEmpty())
-				itp.vider(0);
-		}
-		else {
-			throw new PiocheVideException();
-		}
+	//si pioche contient carte deplace carte du haut de la pioche à la defausse
+	public void deplaceCartePiocheDefausse(Pioche p, InterfaceKlondike itp) {
+		p.getTop().rendVisible();
+		defausse.ajoutCarte(p.removeTop());
+		itp.ajouterUneCarte(defausse.getTop(), 1);
+		if(p.isEmpty())
+			itp.vider(0);
 	}
-	//recycle pioche en deplacant carte du haut de la defausse � la pioche
-	public void recyclePioche(Pioche p, InterfaceKlondike itp) throws RecyclageErrorException, DeplacementImpossibleException{
+	//recycle pioche en deplacant carte du haut de la defausse à la pioche
+	public void recyclePioche(Pioche p, InterfaceKlondike itp) {
 		int nbCarteDefausse=defausse.getNombreCartePaquet();
-		if (p.isEmpty()) {
-			for (int i=0; i<nbCarteDefausse; i++) {
-				p.ajoutCarte(defausse.removeTop());
-				p.getTop().rendCache();
-				itp.ajouterUneCarte(p.getTop(), 0);
-				itp.vider(1);	
-			}
+		for (int i=0; i<nbCarteDefausse; i++) {
+			p.ajoutCarte(defausse.removeTop());
+			p.getTop().rendCache();
+			itp.ajouterUneCarte(p.getTop(), 0);
+			itp.vider(1);	
 		}
-		else
-			throw new RecyclageErrorException();
 	}
 	//retourne vrai si la carte de defausse exp peut etre pose sur pieux recev
-	public boolean deplaceDefPieu(PaquetPieux recev) throws DeplacementImpossibleException {
+	public boolean deplaceDefPieu(PaquetPieux recev) {
 		//si paquet receveur vide renvoie vrai si la carte = as
 		if (recev.isEmpty() && this.defausse.getTop().getValeur().equals(Valeur.as))
 			return true;
@@ -57,23 +47,18 @@ public class Pioche extends Paquet {
 			return true;
 		//si non retourne faux
 		return false;
-
 	}
 	//deplace carte de defausse à pieux
-	public void deplaceDefaussePieu(PaquetPieux recev, InterfaceKlondike itp, int emp) throws DeplacementImpossibleException {
-		if (deplaceDefPieu(recev)) {
-			recev.ajoutCarte(this.defausse.removeTop());
-			itp.ajouterUneCarte(recev.getTop(), emp);
-			if(!defausse.isEmpty())
-				itp.definirCarte(defausse.getTop(), 1);
-			else
-				itp.vider(1);
-		}
+	public void deplaceDefaussePieu(PaquetPieux recev, InterfaceKlondike itp, int emp) {
+		recev.ajoutCarte(this.defausse.removeTop());
+		itp.ajouterUneCarte(recev.getTop(), emp);
+		if (!defausse.isEmpty())
+			itp.definirCarte(defausse.getTop(), 1);
 		else
-			throw new DeplacementImpossibleException();
+			itp.vider(1);
 	}
 	//retourne vrai si la carte de defausse exp peut etre pose sur col recev
-	public boolean deplaceDefCol(PaquetColonne recev) throws DeplacementImpossibleException {
+	public boolean deplaceDefCol(PaquetColonne recev) {
 		//si paquet receveur vide renvoie vrai si la carte = Roi
 		if (recev.isEmpty() && this.defausse.getTop().getValeur().equals(Valeur.roi))
 			return true;
@@ -84,16 +69,12 @@ public class Pioche extends Paquet {
 		return false;
 	}
 	//deplace carte de defausse à colonne
-	public void deplaceDefausseCol(PaquetColonne recev, InterfaceKlondike itp, int emp) throws DeplacementImpossibleException{
-		if (deplaceDefCol(recev)) {
-			recev.ajoutCarte(this.defausse.removeTop());
-			itp.ajouterUneCarte(recev.getTop(), emp);
-			if(!defausse.isEmpty())
-				itp.definirCarte(defausse.getTop(), 1);
-			else
-				itp.vider(1);
-		}
+	public void deplaceDefausseCol(PaquetColonne recev, InterfaceKlondike itp, int emp) {
+		recev.ajoutCarte(this.defausse.removeTop());
+		itp.ajouterUneCarte(recev.getTop(), emp);
+		if (!defausse.isEmpty())
+			itp.definirCarte(defausse.getTop(), 1);
 		else
-			throw new DeplacementImpossibleException();
+			itp.vider(1);
 	}
 }
